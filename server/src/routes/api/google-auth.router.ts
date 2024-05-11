49164b978865622e37b442bc4647cc17f47cfb94
Nazar2015A <nazar.komar456@gmail.com>
2024-05-11 18:22:35 +0300

    change color and set cookie

--- server/src/routes/api/google-auth.router.ts
+++ server/src/routes/api/google-auth.router.ts
@@ -14,14 +14,19 @@ router.get(
     res.cookie("user", req.user, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       secure: true,
-      sameSite: 'none',
-    })
+      sameSite: "none",
+      httpOnly: true,
+    });
     res.redirect(process.env.CLIENT_URL!);
   }
 );
 
 router.get("/logout", (req: Request, res: Response) => {
-  res.clearCookie("user");
+  res.clearCookie("user", {
+    secure: true,
+    sameSite: "none",
+    httpOnly: true,
+  });
   req.logout((err) => {
     if (err) {
       return res.status(500).json({ message: "Error" });
